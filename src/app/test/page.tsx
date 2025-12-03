1880de3d68682767a50b7a5c2a207a6ada2d7f85
BoschiAlbanoJose <boschi.albano.jose@gmail.com>
2025-12-03 17:29:11 -0300

    aprendiendo Git

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,7 +1,5 @@
-import React from 'react'
+import React from "react";
 
 export default function page() {
-  return (
-    <div> Pagina de pruebas</div>
-  )
+  return <div> Aprendiendo Git </div>;
 }
